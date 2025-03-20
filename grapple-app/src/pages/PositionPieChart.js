d411ef975ebd23799012172e6d10646b1dae1066
austinsipp <austin.sipp@gmail.com>
2025-03-20 14:41:36 -0700

    split graph page between attacking and defending

--- grapple-app/src/pages/PositionPieChart.js
+++ grapple-app/src/pages/PositionPieChart.js
@@ -1,10 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef/*, useState*/ } from 'react';
 import * as d3 from 'd3';
 
-const PositionPieChart = ({data_for_viz}) => {
-  
+const PositionPieChart = ({ data_for_viz }) => {
 
-  const d3Container = useRef(null);
+  let attackerData = data_for_viz.filter(element => {
+    return element.position_type === 'attacker'
+  });
+  let defenderData = data_for_viz.filter(element => {
+    return element.position_type === 'defender'
+  });
+
+
+
+  const d3ContainerAttacker = useRef(null);
+  const d3ContainerDefender = useRef(null);
 
   useEffect(() => {
     const width = 300;
@@ -12,11 +21,11 @@ const PositionPieChart = ({data_for_viz}) => {
     const radius = Math.min(width, height) / 2;
 
     // Clear the previous chart before creating a new one
-    const svgContainer = d3.select(d3Container.current);
+    const svgContainer = d3.select(d3ContainerAttacker.current);
     svgContainer.selectAll('*').remove(); // This clears everything inside the container
 
     const svg = d3
-      .select(d3Container.current)
+      .select(d3ContainerAttacker.current)
       .append('svg')
       .attr('width', width)
       .attr('height', height)
@@ -28,8 +37,8 @@ const PositionPieChart = ({data_for_viz}) => {
     const pie = d3.pie().value(d => d.duration);
     const arc = d3.arc().innerRadius(0).outerRadius(radius);
 
-    const pieData = pie(data_for_viz);
-    console.log("Position Chart pieData looks like:",pieData);
+    const pieData = pie(attackerData);
+    console.log("Position Chart pieData looks like:", pieData);
 
     svg
       .selectAll('.arc')
@@ -50,19 +59,74 @@ const PositionPieChart = ({data_for_viz}) => {
       .text(d => d.data.position_desc)
       .style('font-size', '14px')  // Set font size
       .style('fill', 'black');     // Set font color;
-  }, [data_for_viz]);
+  }, [attackerData]);
 
 
 
- 
+
+
+  useEffect(() => {
+    const width = 300;
+    const height = 300;
+    const radius = Math.min(width, height) / 2;
+
+    // Clear the previous chart before creating a new one
+    const svgContainer = d3.select(d3ContainerDefender.current);
+    svgContainer.selectAll('*').remove(); // This clears everything inside the container
+
+    const svg = d3
+      .select(d3ContainerDefender.current)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2},${height / 2})`);
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const pie = d3.pie().value(d => d.duration);
+    const arc = d3.arc().innerRadius(0).outerRadius(radius);
+
+    const pieData = pie(defenderData);
+    console.log("Position Chart pieData looks like:", pieData);
+
+    svg
+      .selectAll('.arc')
+      .data(pieData)
+      .enter()
+      .append('g')
+      .attr('class', 'arc')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', d => color(d.data.position_desc));
+
+    svg
+      .selectAll('.arc')
+      .append('text')
+      .attr('transform', d => `translate(${arc.centroid(d)})`)
+      .attr('dy', '0.35em')
+      .attr('text-anchor', 'middle')
+      .text(d => d.data.position_desc)
+      .style('font-size', '14px')  // Set font size
+      .style('fill', 'black');     // Set font color;
+  }, [defenderData]);
+
+
+
+
 
 
   return <div>
     <h2>Positions</h2>
-      
-     
-      <div ref={d3Container}></div>
-      </div>
+    <div className="attacking-graph">
+      <h3>Attacking</h3>
+      <div ref={d3ContainerAttacker}></div>
+    </div>
+    <div className="defending-graph">
+      <h3>Defending</h3>
+      <div ref={d3ContainerDefender}></div>
+    </div>
+  </div>
 };
 
 export default PositionPieChart;
--- grapple-app/src/pages/SubmissionPieChart.js
+++ grapple-app/src/pages/SubmissionPieChart.js
@@ -1,11 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef/*, useState*/ } from 'react';
 import * as d3 from 'd3';
 
 const SubmissionPieChart = ({ data_for_viz, data_for_filter }) => {
 
+    let attackerData = data_for_viz.filter(element => {
+        return element.position_type === 'attacker'
+    });
+    let defenderData = data_for_viz.filter(element => {
+        return element.position_type === 'defender'
+    });
 
-
-    const d3Container = useRef(null);
+    const d3ContainerAttacker = useRef(null);
+    const d3ContainerDefender = useRef(null);
 
     useEffect(() => {
         const width = 300;
@@ -13,11 +19,11 @@ const SubmissionPieChart = ({ data_for_viz, data_for_filter }) => {
         const radius = Math.min(width, height) / 2;
 
         // Clear the previous chart before creating a new one
-        const svgContainer = d3.select(d3Container.current);
+        const svgContainer = d3.select(d3ContainerAttacker.current);
         svgContainer.selectAll('*').remove(); // This clears everything inside the container
 
         const svg = d3
-            .select(d3Container.current)
+            .select(d3ContainerAttacker.current)
             .append('svg')
             .attr('width', width)
             .attr('height', height)
@@ -29,7 +35,7 @@ const SubmissionPieChart = ({ data_for_viz, data_for_filter }) => {
         const pie = d3.pie().value(d => d.count);
         const arc = d3.arc().innerRadius(0).outerRadius(radius);
 
-        const pieData = pie(data_for_viz);
+        const pieData = pie(attackerData);
         console.log("Submission Chart pieData looks like:", pieData);
 
         svg
@@ -48,19 +54,64 @@ const SubmissionPieChart = ({ data_for_viz, data_for_filter }) => {
             .attr('transform', d => `translate(${arc.centroid(d)})`)
             .attr('dy', '0.35em')
             .text(d => d.data.position_desc);
-    }, [data_for_viz]);
+    }, [attackerData]);
 
+    useEffect(() => {
+        const width = 300;
+        const height = 300;
+        const radius = Math.min(width, height) / 2;
 
+        // Clear the previous chart before creating a new one
+        const svgContainer = d3.select(d3ContainerDefender.current);
+        svgContainer.selectAll('*').remove(); // This clears everything inside the container
+
+        const svg = d3
+            .select(d3ContainerDefender.current)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g')
+            .attr('transform', `translate(${width / 2},${height / 2})`);
+
+        const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+        const pie = d3.pie().value(d => d.count);
+        const arc = d3.arc().innerRadius(0).outerRadius(radius);
+
+        const pieData = pie(defenderData);
+        console.log("Submission Chart pieData looks like:", pieData);
+
+        svg
+            .selectAll('.arc')
+            .data(pieData)
+            .enter()
+            .append('g')
+            .attr('class', 'arc')
+            .append('path')
+            .attr('d', arc)
+            .attr('fill', d => color(d.data.position_desc));
+
+        svg
+            .selectAll('.arc')
+            .append('text')
+            .attr('transform', d => `translate(${arc.centroid(d)})`)
+            .attr('dy', '0.35em')
+            .text(d => d.data.position_desc);
+    }, [defenderData]);
 
 
 
 
     return <div>
-        <h2>Submissions Achieved</h2>
-
-
-        {/* D3 Pie Chart */}
-        <div ref={d3Container}></div>
+        <h2>Submissions</h2>
+        <div className="attacking-graph">
+            <h3>Attacking</h3>
+            <div ref={d3ContainerAttacker}></div>
+        </div>
+        <div className="defending-graph">
+            <h3>Defending</h3>
+            <div ref={d3ContainerDefender}></div>
+        </div>
     </div>
 };
 
